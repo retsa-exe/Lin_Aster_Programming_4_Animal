bdd95476797bfd2502712c46cb1c2e49dde95fce
retsa-exe <asterlin7890@gmail.com>
2026-02-02 15:47:37 -0500

    Finished animation experiment

---
+++ Assets/Scripts/MoveToAT.cs
@@ -0,0 +1,54 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class MoveToAT : ActionTask {
+
+		public BBParameter<Animator> hamsterAnimator;
+
+		public Transform targetTransform;
+
+		public NavMeshAgent navMeshAgent;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate()
+		{
+			navMeshAgent.SetDestination(targetTransform.position);
+
+			if (Vector3.Distance(targetTransform.position, navMeshAgent.transform.position) < 0.3f)
+			{
+				hamsterAnimator.value.SetBool("isWalking", false);
+            }
+			else
+			{
+				hamsterAnimator.value.SetBool("isWalking", true);
+            }
+		}
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
