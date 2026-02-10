b66c10a4189c132ac38768d394729a055ad11c55
retsa-exe <asterlin7890@gmail.com>
2026-02-10 14:49:51 -0500

    Finished Week 5 session 3

---
+++ Assets/Scripts/DayCycleAT.cs
@@ -0,0 +1,49 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class DayCycleAT : ActionTask {
+
+		public BBParameter<bool> isDayTime;
+		public float dayDuration;
+        float time;
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit() {
+			time = 0;
+            return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+			time += Time.deltaTime;
+			Debug.Log("Time: " + time);
+            if (time >= dayDuration)
+			{
+				isDayTime.SetValue(!isDayTime.value);
+				time = 0;
+            }
+		}
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
